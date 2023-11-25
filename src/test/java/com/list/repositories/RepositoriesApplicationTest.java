b8dca108c3945022a63d71aa56f99fbaa957c3e8
Greshma John <greshmaj99@gmail.com>
2023-11-26 00:42:30 +0100

    readMe file

--- src/test/java/com/list/repositories/RepositoriesApplicationTest.java
+++ src/test/java/com/list/repositories/RepositoriesApplicationTest.java
@@ -1,11 +1,11 @@
-//package com.list.repositories;
-//
-//import org.junit.jupiter.api.Test;
-// class RepositoriesApplicationTest{
-//	
-//	 @Test
-//	  void main() {
-//		RepositoriesApplication.main(new String[] {});
-//	 }
-//	
-//}
+package com.list.repositories;
+
+import org.junit.jupiter.api.Test;
+ class RepositoriesApplicationTest{
+	
+	 @Test
+	  void main() {
+		RepositoriesApplication.main(new String[] {});
+	 }
+	
+}
